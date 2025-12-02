b3005d28c3a94516a30e852cd31492db29f1ac41
Clint <clintcalhoun@gmail.com>
2025-12-02 12:44:29 -0600

    feat: Add 'Give' link to menu, styled as a button

--- public/menu.php
+++ public/menu.php
@@ -193,6 +193,12 @@
                     <span class="en">About us</span><span class="es">Acerca de Nosotros</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="/give/" class="nav-link button button-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                    <span class="en">Give</span><span class="es">Donar</span>
+                </a>
+            </li>
         </ul>
     </div>
 
